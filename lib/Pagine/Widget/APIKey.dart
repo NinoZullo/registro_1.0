23a109c87d14850b206e07944636de2def92e9cc
Nino Zullo <nino.zullo@galileo.galileicrema.it>
2023-06-06 11:23:54 +0200

    Fine progetto, ottimizzazione e miglioramento grafica

---
+++ lib/Pagine/Widget/APIKey.dart
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2023. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+//Pagina terminata ed Ottimizzata ✅
+
+class APIKey {
+  static const apiKey = "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";
+}
